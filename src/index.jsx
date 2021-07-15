ec0842a95704c92456fb784ac9ca5e6fdb3ccbfc
Aldo <aldocape@gmail.com>
2021-07-15 08:26:28 -0300

    Instalación e inicialización de la app

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return <p>Hola!</p>;
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
